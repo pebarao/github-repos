d6dd549797607fb05c543b2893fba2546708caa8
Felipe Barão <pebaraoo@gmail.com>
2022-06-10 08:37:35 -0300

    add fast refresh plugin to webpack and styles to repositoryList component

--- src/components/RepositotyList.jsx
+++ src/components/RepositotyList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { RepositoryItem } from './RepositoryItem'
-
+import '../styles/repositories.scss'
 export function RepositoryList(){
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-       <RepositoryItem repository={Unform}/>
+       <RepositoryItem />
        <RepositoryItem />
        <RepositoryItem />
       </ul>
