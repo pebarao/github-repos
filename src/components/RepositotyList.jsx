41ff198ddef64bc54ec93488d979c6276b2de2c9
Felipe Barão <pebaraoo@gmail.com>
2022-06-10 09:04:26 -0300

    feat: add fetch from github

--- src/components/RepositotyList.jsx
+++ src/components/RepositotyList.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect }   from 'react'
 import React from 'react'
+
 import { RepositoryItem } from './RepositoryItem'
 import '../styles/repositories.scss'
+
+
 export function RepositoryList(){
+  const [repository, setRepository] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/pebaraoo/repos")
+    .then(response => response.json())
+    .then(data => setRepository(data))
+  }, [])
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
