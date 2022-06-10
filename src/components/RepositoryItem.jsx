4a97f83f03eaeba3157f94d8a0e5124bb7bc928f
Felipe Barão <pebaraoo@gmail.com>
2022-06-10 08:23:58 -0300

    feat: add RepositoryItem component

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export function RepositoryItem() {
+  return (
+    <li>
+      <strong>Unform</strong>
+      <p>Forms in react</p>
+
+      <a href="#"> acessar repositório</a>
+    </li>
+  )
+}
--- src/components/RepositotyList.jsx
+++ src/components/RepositotyList.jsx
@@ -1,34 +1,14 @@
 import React from 'react'
+import { RepositoryItem } from './RepositoryItem'
 
 export function RepositoryList(){
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <li>
-          <strong>Unform</strong>
-          <p>Forms in react</p>
-
-          <a href="#"> acessar repositório</a>
-        </li>
-        <li>
-          <strong>Unform</strong>
-          <p>Forms in react</p>
-
-          <a href="#"> acessar repositório</a>
-        </li>
-        <li>
-          <strong>Unform</strong>
-          <p>Forms in react</p>
-
-          <a href="#"> acessar repositório</a>
-        </li>
-        <li>
-          <strong>Unform</strong>
-          <p>Forms in react</p>
-
-          <a href="#"> acessar repositório</a>
-        </li>
+       <RepositoryItem repository={Unform}/>
+       <RepositoryItem />
+       <RepositoryItem />
       </ul>
     </section>
   )
